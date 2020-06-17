4ebd60049d0c6a98cf5b3b0f8b486dd377edab3b
Samuel Gulliksson <samuel.gulliksson@gmail.com>
2020-06-17 21:22:55 +0200

    Add support for switching between roles for a logged-in user.
    Also add support for configuring a prefix for the S3 bucket per role, to
    allow more fine-grained role permissions (i.e. role that only has access
    to "subfolder" of bucket).

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+import dataclasses
 import logging
 
+import boto3
+import botocore
 import flask
 from flask import Flask, request, redirect, render_template, url_for
 from flask_pyoidc import OIDCAuthentication
@@ -44,14 +47,46 @@ def index():
         flask.session['mfa-challenge'] = challenge
         return redirect(url_for('verify_mfa'), code=303)
 
-    boto3_session = aws_accesser.get_boto3_session(user_session.id_token_jwt, app.config['ROLE_ARN'])
+    bucket_content = []
+    if 'aws-credentials' in flask.session:
+        session = boto3.Session(**flask.session['aws-credentials'])
+        bucket_prefix = app.config['S3_PREFIX_PER_ROLE'].get(flask.session.get('aws-role', None))
+        try:
+            bucket_content = [obj.key for obj in s3.list_bucket(session, app.config['S3_BUCKET_NAME'], bucket_prefix)]
+        except botocore.exceptions.ClientError as error:
+            flask.flash('You don\'t have access')
+
     return render_template(
         'index.html',
         user_groups=user_session.id_token['cognito:groups'],
         user_roles=user_session.id_token['cognito:roles'],
         s3_bucket=app.config['S3_BUCKET_NAME'],
-        s3_bucket_content=[obj.key for obj in s3.list_bucket(boto3_session, app.config['S3_BUCKET_NAME'])]
+        s3_bucket_content=bucket_content
     )
+
+
+@app.route('/switch-role', methods=['POST'])
+def switch_role():
+    requested_role = request.form['role']
+
+    if not requested_role:
+        if 'aws-credentials' in flask.session:
+            del flask.session['aws-credentials']
+            del flask.session['aws-role']
+            flask.flash('No role assumed')
+        return redirect(url_for('index'), code=303)
+
+    user_session = UserSession(flask.session)
+    if requested_role not in user_session.id_token.get('cognito:roles', []):
+        flask.flash(f'Failed to switch role')
+        return redirect(url_for('index'), code=303)
+
+    aws_credentials = aws_accesser.get_credentials(user_session.id_token_jwt, requested_role)
+    flask.session['aws-credentials'] = dataclasses.asdict(aws_credentials)
+    flask.session['aws-role'] = requested_role
+
+    flask.flash(f'Switched role to {requested_role}')
+    return redirect(url_for('index'), code=303)
 
 
 @app.route('/verify-mfa', methods=['GET', 'POST'])
--- cognitodemo/s3.py
+++ cognitodemo/s3.py
@@ -1,4 +1,7 @@
-def list_bucket(session, bucket_name):
+def list_bucket(session, bucket_name, prefix=None):
     s3 = session.resource('s3')
     bucket = s3.Bucket(bucket_name)
+    if prefix:
+        return bucket.objects.filter(Prefix=prefix)
+
     return bucket.objects.all()
